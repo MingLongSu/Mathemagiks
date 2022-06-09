3631a16335b90a0941fc11f41ae964a5e7ec64a2
MingLongSu <bananafrenzy31@gmail.com>
2022-06-08 23:31:31 -0400

    Added some colours to the theme for project

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -15,25 +15,42 @@ class Mathemagiks extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const MyMathemagiks(title: 'Mathemagiks'),
+      home: const MyMathemagiks(
+        title: 'Mathemagiks', 
+        primColor: Color.fromARGB(255, 147, 207, 215),
+        secColor: Color.fromARGB(255, 199, 151, 170), 
+        tertColor: Color.fromARGB(255, 221, 222, 214),
+        primTextColor: Color.fromARGB(255, 85, 172, 205),
+        secTextColor: Color.fromARGB(255, 230, 227, 220)
+      ),
     );
   }
 }
 
 class MyMathemagiks extends StatefulWidget {
-  const MyMathemagiks({Key? key, required this.title}) : super(key: key);
+  const MyMathemagiks({ 
+    Key? key, 
+    required this.title, 
+    required this.primColor,
+    required this.secColor,
+    required this.tertColor,
+    required this.primTextColor,
+    required this.secTextColor
+  }) : super(key: key);
 
   final String title;
+  final Color primColor, secColor, tertColor;
+  final Color primTextColor, secTextColor;
 
   @override
   State<MyMathemagiks> createState() => _MyMathemagiksState();
 }
 
-class _MyMathemagiksState extends State<MyMathemagiks> {
+class _MyMathemagiksState extends State<MyMathemagiks> { 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: widget.primColor,
     );
   }
 }
